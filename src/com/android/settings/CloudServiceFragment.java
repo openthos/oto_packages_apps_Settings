851e83bc8c8a62794ed7bf7136b29d525a4c099f
Liu Xiaoxu <823090683@qq.com>
2017-02-16 11:08:37 +0000

    Setting: Add feature
    The CloudService can import and export the imageWallpaper.
    
    Also remove redundant code.
    
    Signed-off-by: Liu Xiaoxu <823090683@qq.com>
    Reviewed-by: Chen Gang <chengang@emindsoft.com.cn>

--- src/com/android/settings/CloudServiceFragment.java
+++ src/com/android/settings/CloudServiceFragment.java
@@ -5,63 +5,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.android.settings.applications.*;
 import android.widget.Switch;
-import android.content.Intent;
-import android.content.Context;
-import android.content.BroadcastReceiver;
-import android.content.IntentFilter;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.io.InputStream;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
-import android.widget.CompoundButton;
 import com.android.settings.widget.SwitchBar;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.util.Log;
-import android.widget.Toast;
 import android.text.TextUtils;
-import android.os.Environment;
 import android.net.Uri;
 import android.database.Cursor;
+import android.app.WallpaperManager;
 
-public class CloudServiceFragment extends Fragment implements OnCheckedChangeListener {
-    private Switch mswitch_wallpaper;
-    private Switch mswitch_wifi;
-    private Switch mswitch_email;
-    //private Switch mswitch_photo;
-    private Switch mswitch_appdata;
-    private Switch mswitch_appstore;
-    private Switch mswitch_browser;
-    private Switch mswitch_startmenu;
-    //private Switch mswitch_file;
+public class CloudServiceFragment extends Fragment implements OnClickListener {
+    private Switch mSwitchWallpaper;
+    private Switch mSwitchWifi;
+    private Switch mSwitchEmail;
+    private Switch mSwitchAppdata;
+    private Switch mSwitchAppstore;
+    private Switch mSwitchBrowser;
+    private Switch mSwitchStartupmenu;
 
-    private Button mbutton_import;
-    private Button mbutton_export;
+    private Button mButtonImport;
+    private Button mButtonExport;
 
-    private String oldPath;
-    private String newPath;
+    private String mWallpaperOldPath = "data/system/users/0/wallpaper";
+    private String mWallpaperNewPath = "data/sea/data/sdcard/cloudFolder/wallpaper";
 
-    private Intent intent;
-    private cloudReceiver mCloudReceiver;
-    public static final String BROADCAST_ACTION = "sendBroadcast";
-    public static final String BROADCAST_WALLPAPER = "sendWallpaperBroadcast";
-    public static final String BROADCAST_WIFI = "sendWifiBroadcast";
-    public static final String BROADCAST_EMAIL = "sendEmailBroadcast";
-    //public static final String BROADCAST_PHOTO = "sendPhotoBroadcasT";
-    public static final String BROADCAST_STARTMENU = "sendStartmenuBroadcast";
-    public static final String BROADCAST_BROWSER = "sendbrowserBroadcast";
-    public static final String BROADCAST_APPSTORE= "sendAppstoreBroadcast";
-    public static final String BROADCAST_APPDATA = "sendAppdataBroadcast";
-    //public static final String BROADCAST_FILE = "sendFileBroadcast";
-    private static final String CLOUDSERVICE_ROOT_PATH = "data/sea/data/sdcard/cloudFolder/";
+    private File mCloudFolder;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -70,144 +46,86 @@ public class CloudServiceFragment extends Fragment implements OnCheckedChangeLis
         View mRootView = inflater.inflate(R.layout.fragment_cloud_service, container, false);
         initView(mRootView);
 
-        mCloudReceiver = new cloudReceiver();
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(BROADCAST_WALLPAPER);
-        intentFilter.addAction(BROADCAST_WIFI);
-        intentFilter.addAction(BROADCAST_STARTMENU);
-        getActivity().registerReceiver(mCloudReceiver, intentFilter);
-
-        File dirFolder = new File(CLOUDSERVICE_ROOT_PATH);
-        if (!dirFolder.exists()) {
-            dirFolder.mkdirs();
-        }
-        //File fileWallpaperName = new File("data/sea/data/sdcard/cloudFolder/CloudWallpaper.xml");
-        File fileWifiName = new File(CLOUDSERVICE_ROOT_PATH + "CloudWifi.xml");
-        File fileEmailName = new File(CLOUDSERVICE_ROOT_PATH + "CloudEmail.xml");
-        File fileAppdataName = new File(CLOUDSERVICE_ROOT_PATH + "CloudAppdata.xml");
-        //File filePictureName = new File("data/sea/data/sdcard/cloudFolder/CloudPicture.xml");
-        File fileStartmenuName = new File(CLOUDSERVICE_ROOT_PATH + "CloudStartMenu.xml");
-        File fileAppStoreName = new File(CLOUDSERVICE_ROOT_PATH + "CloudAppStore.xml");
-        File fileBrowserName = new File(CLOUDSERVICE_ROOT_PATH + "CloudBrowser.xml");
-        //File fileFileName = new File("data/sea/data/sdcard/cloudFolder/CloudFile.xml");
-
-        try {
-            //if (!fileWallpaperName.exists()) {
-            //    fileWallpaperName.createNewFile();
-            //}
-            if (!fileWifiName.exists()) {
-                fileWifiName.createNewFile();
-            }
-            if (!fileEmailName.exists()) {
-                fileEmailName.createNewFile();
-            }
-            if (!fileAppdataName.exists()) {
-                fileAppdataName.createNewFile();
-            }
-            //if (!filePictureName.exists()) {
-            //    filePictureName.createNewFile();
-            //}
-            if (!fileStartmenuName.exists()) {
-                fileStartmenuName.createNewFile();
-            }
-            if (!fileAppStoreName.exists()) {
-                fileAppStoreName.createNewFile();
-            }
-            if (!fileBrowserName.exists()) {
-                fileBrowserName.createNewFile();
-            }
-        } catch (IOException e) {
-           // TODO Auto-generated catch block
-           e.printStackTrace();
-        }
+        mCloudFolder = new File(getActivity().getResources()
+                                             .getString(R.string.cloudfolder_path));
         return mRootView;
     }
 
     private void initView(View v) {
-        mswitch_wallpaper = (Switch) v.findViewById(R.id.switch_wallpaper);
-        mswitch_wifi = (Switch) v.findViewById(R.id.switch_wifi);
-        mswitch_email = (Switch) v.findViewById(R.id.switch_email);
-        //mswitch_photo = (Switch) v.findViewById(R.id.switch_photo);
-        mswitch_appdata = (Switch) v.findViewById(R.id.switch_appdata);
-        mswitch_appstore = (Switch) v.findViewById(R.id.switch_appstore);
-        mswitch_browser = (Switch) v.findViewById(R.id.switch_browser);
-        mswitch_startmenu = (Switch) v.findViewById(R.id.switch_startmenu);
-        //mswitch_file = (Switch) v.findViewById(R.id.switch_file);
+        mSwitchWallpaper = (Switch) v.findViewById(R.id.switch_wallpaper);
+        mSwitchWifi = (Switch) v.findViewById(R.id.switch_wifi);
+        mSwitchEmail = (Switch) v.findViewById(R.id.switch_email);
+        mSwitchAppdata = (Switch) v.findViewById(R.id.switch_appdata);
+        mSwitchAppstore = (Switch) v.findViewById(R.id.switch_appstore);
+        mSwitchBrowser = (Switch) v.findViewById(R.id.switch_browser);
+        mSwitchStartupmenu = (Switch) v.findViewById(R.id.switch_startmenu);
 
-        mbutton_import = (Button) v.findViewById(R.id.cloud_import);
-        mbutton_export = (Button) v.findViewById(R.id.cloud_export);
+        mButtonImport = (Button) v.findViewById(R.id.cloud_import);
+        mButtonExport = (Button) v.findViewById(R.id.cloud_export);
 
-        mswitch_wallpaper.setOnCheckedChangeListener(this);
-        mswitch_wifi.setOnCheckedChangeListener(this);
-        mswitch_email.setOnCheckedChangeListener(this);
-        //mswitch_photo.setOnCheckedChangeListener(this);
-        mswitch_appdata.setOnCheckedChangeListener(this);
-        mswitch_appstore.setOnCheckedChangeListener(this);
-        mswitch_browser.setOnCheckedChangeListener(this);
-        mswitch_startmenu.setOnCheckedChangeListener(this);
-        //mswitch_file.setOnCheckedChangeListener(this);
-
-        //mbutton_import.setOnClickListener(this);
-        //mbutton_export.setOnClickListener(this);
+        mButtonImport.setOnClickListener(this);
+        mButtonExport.setOnClickListener(this);
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
-        // TODO Auto-generated method stub
-        intent = new Intent();
-        intent.setAction(BROADCAST_ACTION);
-        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-        // TODO Auto-generated method stub
-        switch (buttonView.getId()) {
-            case R.id.switch_wallpaper:
-                intent.setAction(BROADCAST_WALLPAPER);
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.cloud_import:
+                importWallpaperFiles();
                 break;
-            case R.id.switch_wifi:
-                intent.setAction(BROADCAST_WIFI);
+            case R.id.cloud_export:
+                if (!mCloudFolder.exists()) {
+                    mCloudFolder.mkdirs();
+                }
+                if (mSwitchWallpaper.isChecked()) {
+                    exportWallpaperFiles();
+                }
+                if (mSwitchStartupmenu.isChecked()) {
+                    exportStartupmenuFiles();
+                }
                 break;
-            case R.id.switch_email:
-                intent.setAction(BROADCAST_EMAIL);
-                break;
-            //case R.id.switch_photo:
-                //intent.setAction(BROADCAST_PHOTO);
-                //break;
-            case R.id.switch_appdata:
-                intent.setAction(BROADCAST_APPDATA);
-                break;
-            case R.id.switch_appstore:
-                intent.setAction(BROADCAST_APPSTORE);
-                break;
-            case R.id.switch_browser:
-                intent.setAction(BROADCAST_BROWSER);
-                break;
-            case R.id.switch_startmenu:
-                intent.setAction(BROADCAST_STARTMENU);
-                break;
-            //case R.id.switch_file:
-                //intent.setAction(BROADCAST_FILE);
-                //break;
         }
-        getActivity().sendBroadcast(intent);
     }
 
-    public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
+    private void importWallpaperFiles() {
+        if (mSwitchWallpaper.isChecked()) {
+            InputStream wallpaperFile = null;
+            if (mCloudFolder.exists()){
+                try {
+                    wallpaperFile = new FileInputStream(mWallpaperNewPath);
+                    if (wallpaperFile != null) {
+                        WallpaperManager manager =
+                                         WallpaperManager.getInstance(getActivity());
+                        manager.setStream(wallpaperFile);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (wallpaperFile !=null) {
+                            wallpaperFile.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 
-    public static boolean copyFile(String oldPath, String newPath) {
-        boolean iscopy = false;
+    public void exportWallpaperFiles() {
         InputStream inStream = null;
         FileOutputStream fs = null;
         try {
-            int byteread = 0;
-            File oldfile = new File(oldPath);
-            if (oldfile.exists()) {
-                inStream = new FileInputStream(oldPath);
-                fs = new FileOutputStream(newPath);
+            int byteRead = 0;
+            File wallpaperOldFile = new File(mWallpaperOldPath);
+            if (wallpaperOldFile.exists()) {
+                inStream = new FileInputStream(mWallpaperOldPath);
+                fs = new FileOutputStream(mWallpaperNewPath);
                 byte[] buffer = new byte[1024];
-                while ((byteread = inStream.read(buffer)) != -1) {
-                    fs.write(buffer, 0, byteread);
+                while ((byteRead = inStream.read(buffer)) != -1) {
+                    fs.write(buffer, 0, byteRead);
                 }
-                iscopy = true;
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -227,7 +145,20 @@ public class CloudServiceFragment extends Fragment implements OnCheckedChangeLis
                 e.printStackTrace();
             }
         }
-        return iscopy;
+    }
+
+    private void exportStartupmenuFiles() {
+        Uri uriQuery = Uri.parse(
+               "content://com.android.systemui.util.StatusBarContentProvider/status_bar_tb");
+        if (uriQuery != null) {
+            Cursor cursor = getActivity().getContentResolver().
+                                       query(uriQuery, null, null, null, null);
+            if (cursor != null && cursor.moveToNext()) {
+                writeFile("data/data/cloudFolder/CloudStartMenu.xml",
+                               cursor.getString(cursor.getColumnIndex("pkgname")) + "\n", true);
+                cursor.close();
+            }
+        }
     }
 
     public static boolean writeFile(String fileName, String content, boolean append) {
@@ -259,36 +190,5 @@ public class CloudServiceFragment extends Fragment implements OnCheckedChangeLis
                 }
             }
         }
-    }
-
-    public class cloudReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            switch (intent.getAction()) {
-                case BROADCAST_WALLPAPER:
-                    oldPath = "data/system/users/0/wallpaper";
-                    newPath = CLOUDSERVICE_ROOT_PATH + "wallpaper";
-                    copyFile (oldPath,newPath);
-                    break;
-                case BROADCAST_STARTMENU:
-                    Uri uriQuery = Uri.parse(
-                      "content://com.android.systemui.util.StatusBarContentProvider/status_bar_tb");
-                    if (uriQuery != null) {
-                        Cursor cursor = getActivity().getContentResolver().
-                                   query(uriQuery, null, null, null, null);
-                        if (cursor != null && cursor.moveToNext()) {
-                            writeFile(CLOUDSERVICE_ROOT_PATH + "CloudStartMenu.xml",
-                                   cursor.getString(cursor.getColumnIndex("pkgname")) + "\n", true);
-                            cursor.close();
-                        }
-                    }
-                    break;
-            }
-        }
-    }
-
-    public void onDestroy() {
-        getActivity().unregisterReceiver(mCloudReceiver);
-        super.onDestroy();
     }
 }
