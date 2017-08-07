57ce98cfead6dca69d085ee8e52f2fa27639afd9
Luo Junhuan <forward_m@126.com>
2017-08-07 07:46:01 +0000

    Settings: Remove a broadcast receiver whose action is "Intent.STATUS_BAR_WIFI_ICON"
    Signed-off-by: Luo Junhuan <forward_m@126.com>
    Signed-off-by: Wang Zhixu <potatomagic@163.com>

--- src/com/android/settings/wifi/WifiSettingsForSetupWizard.java
+++ src/com/android/settings/wifi/WifiSettingsForSetupWizard.java
@@ -89,10 +89,7 @@ public class WifiSettingsForSetupWizard extends WifiSettings {
                 Context.CONNECTIVITY_SERVICE);
         mEthDialog = new EthernetDialog(getActivity(),(EthernetManager)
                                         getSystemService(Context.ETHERNET_SERVICE),mCM);
-        Intent intentEthnet = new Intent();
-        intentEthnet.setAction(Intent.STATUS_BAR_WIFI_ICON);
         if (isEthernet(mCM)) {
-            getActivity().sendBroadcast(intentEthnet);
             mEthernetState.setText(getText(R.string.network_is_available));
         } else {
             mEthernetState.setText(getText(R.string.network_is_not_available));
