120d3b1fdcb70d242955ae76fb38beb924882c2b
Liu Xiaoxu <18600956771@163.com>
2018-03-27 11:09:46 +0000

    Settings: Modify the Regex to show the kernel version
    Signed-off-by: Liu Xiaoxu <18600956771@163.com>
    Reviewed-by: Wang Zhixu <wangzhixu@openthos.org>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -519,7 +519,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements
         final String PROC_VERSION_REGEX =
             "Linux version (\\S+) " + /* group 1: "3.0.31-g6fb96c9" */
             "\\((\\S+?)\\) " +        /* group 2: "x@y.com" (kernel builder) */
-            "(?:\\(gcc.+? \\)) " +    /* ignore: GCC version information */
+            "(?:\\(gcc.+?\\)) " +     /* ignore: GCC version information */
             "(#\\d+) " +              /* group 3: "#1" */
             "(?:.*?)?" +              /* ignore: optional SMP, PREEMPT, and any CONFIG_FLAGS */
             "((Sun|Mon|Tue|Wed|Thu|Fri|Sat).+)"; /* group 4: "Thu Jun 28 11:02:39 PDT 2012" */
