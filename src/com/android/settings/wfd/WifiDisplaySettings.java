def7ee45c5692edb1d71d5d375cf6a927f63306c
ladehunter <ladehunter@gmail.com>
2018-05-11 02:29:25 +0000

    Fix setting crash bug causing by lose wifiP2Pconfig.
    Signed-off-by: ladehunter <ladehunter@gmail.com>

--- src/com/android/settings/wfd/WifiDisplaySettings.java
+++ src/com/android/settings/wfd/WifiDisplaySettings.java
@@ -213,13 +213,14 @@ public final class WifiDisplaySettings extends SettingsPreferenceFragment {
     }
 
     public static boolean isAvailable(Context context) {
-        try {
-            return context.getSystemService(Context.DISPLAY_SERVICE) != null
-                    && context.getSystemService(Context.WIFI_P2P_SERVICE) != null;
-        } catch (Exception e) {
-            // Service is not registered, so this is definitely not available.
-            return false;
-        }
+        return false;
+        //try {
+        //    return context.getSystemService(Context.DISPLAY_SERVICE) != null
+        //            && context.getSystemService(Context.WIFI_P2P_SERVICE) != null;
+        //} catch (Exception e) {
+        //    // Service is not registered, so this is definitely not available.
+        //    return false;
+        //}
     }
 
     private void scheduleUpdate(int changes) {
